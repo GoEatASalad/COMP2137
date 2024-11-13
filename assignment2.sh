e707fcd6ea513766148ae4ab278f199908c70d5f
Mackenzie Horgan <bigmack1899@gmail.com>
2024-11-13 18:39:03 -0500

    Added install for squid.

--- assignment2.sh
+++ assignment2.sh
@@ -13,10 +13,26 @@ else
 		exit 1
 	fi
 	# Installs apache2. If it fails, the user is given an error message and the script exits.
-	if ! sudo apt install apache2; then
+	if ! sudo apt install -y apache2; then
 		echo "Failed to install apache2."
 		exit 1
 	fi
 	# If the install is successful, the user is given a message.
 	echo "Apache2 is now installed."
+fi
+
+# Test to see if squid is installed.
+# If true, returns a message to the user.
+if dpkg -l squid; then
+	echo "Squid is already installed."
+# If false, tells the user squid will now be installed.
+else
+	echo "Squid is not installed, installing now."
+	# Installs squid. If it fails, the user is given an error message and the script exits.
+	if ! sudo apt install -y squid; then
+		echo "Failed to install squid."
+		exit 1
+	fi
+	# If the install is successful, the user is given a message.
+	echo "Squid is now installed."
 fi
