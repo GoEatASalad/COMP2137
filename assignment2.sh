c7a553dc1297517b2b93ff0b9f77f8531bc9064a
Mackenzie Horgan <bigmack1899@gmail.com>
2024-11-13 19:54:56 -0500

    Added script to add user aubrey

--- assignment2.sh
+++ assignment2.sh
@@ -36,3 +36,15 @@ else
 	# If the install is successful, the user is given a message.
 	echo "Squid is now installed."
 fi
+
+
+# Create new user accounts
+# Create aubrey account if one doesn't exist.
+if ! id "aubrey" &>/dev/null; then
+	# creates and specifies the user's home directory, and specifies bash as the default shell. Returns a success message to the user.
+	sudo useradd -m -d /home/aubrey -s /bin/bash aubrey
+	echo "The user 'aubrey' has been added. Their home directory is /home/aubrey"
+# If the account already exists, the user is informed.
+else
+	echo "User 'aubrey' already exists."
+fi
